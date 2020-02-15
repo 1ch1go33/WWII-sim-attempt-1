49938322caaedebe346cfbde09f9fa031dca2941
1ch1go33 <57801740+1ch1go33@users.noreply.github.com>
2020-02-15 12:39:20 -0600

    Update testpygame.py

--- testpygame.py
+++ testpygame.py
@@ -7,46 +7,50 @@ pygame.init()
 #assign several in game variables
 white = (255, 255, 255) 
 black= (0,0,0)
-wi = 400  #change these two to fit game
+width = 400  #change these two to fit game
 height = 400
-war==True
+war=True
 allies=[]
 enemies=[]
 neutral=[]
 #initiate the pygame window
-screen = pygame.display.set_mode((width, height)) 
+Screen = pygame.display.set_mode((width, height)) 
   
 # set the pygame window name 
 pygame.display.set_caption('World War II simulator') 
 
-image = pygame.image.load(r'')#insert file path and image name
-def Run_Game():
+image = pygame.image.load(r'images\BlackSquare.png')#insert file path and image name
+SpriteList=[image]
+#def LoadSprites():
+    #for sprite in SpriteList:
+      #  Screen.blit(sprite, (0,0))
+def RunGame():
 # conditional loop
-while war==True : 
+    while war==True : 
   
-    # completely fill the surface object 
-    display_surface.fill(white) 
+        # completely fill the surface object 
+        Screen.fill(white) 
   
     # copying the image surface object 
     # to the display surface object at 
     # (0, 0) coordinate. 
-    display_surface.blit(image, (0, 0)) 
+        Screen.blit(image, (0, 0)) 
   
     # iterate over the list of Event objects 
     # that was returned by pygame.event.get() method. 
-    for event in pygame.event.get() : 
+        for event in pygame.event.get() : 
   
         # if event object type is QUIT 
         # then quitting the pyame 
         # and program both. 
-        if event.type == pygame.QUIT : 
+            if event.type == pygame.QUIT : 
   
             # deactivates the pygame library 
-            pygame.quit() 
+                pygame.quit() 
   
             # quit the program. /
-            quit() 
+                quit() 
   
         # Draws the surface object to the screen.   
         pygame.display.update()  
-             
+RunGame()
