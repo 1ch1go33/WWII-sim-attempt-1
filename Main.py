1d4eaa4d7c610ddcc931962cd20e065ca55e930b
1ch1go33 <57801740+1ch1go33@users.noreply.github.com>
2020-02-22 15:57:35 -0600

    replaced the Objects and Sprite List with a more efficient loop

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import pygame
 import time
 import math
 import os
-from array import *
+#from array import *
 pygame.init()
 
 white = (255, 255, 255)
@@ -13,10 +13,6 @@ war=False
 allies=[]
 enemies=[]
 neutral=[]
-#Objects = array('i', [1, 2, 3,]) this needs further testing
-Objects = []#inside of each object, have an x, y, width, and height
-#example = Objects = [[600, 420, 300, 100], [150, 420, 300, 100]]
-#use for loop to find out where mouse is and where clickable box is
 TimeDelay = 0
 WallpaperCycle = 0
 #whenever you load an image, put a comment of the dimensions right next to it
@@ -25,31 +21,28 @@ StartGameB = pygame.image.load(r'images\StartGame.png') #(300, 100)
 TitleScreen = pygame.image.load(r'images\WorldWarIITitleScreen.png') #(600, 1000)
 TitleScreenStalin = pygame.image.load(r'images\wwII_3.png') #(600, 1000)
 TitleScreenHitler = pygame.image.load(r'images\wwII_1.png') #(600, 1000)
-LoadGameR= LoadGameB.get_rect(topleft=(600, 420))
 #TitleScreenFDR = pygame.image.load(r'')
 #initiate the pygame window
 Screen = pygame.display.set_mode((width, height))
-
+# set up hitboxes
+LoadGameR= LoadGameB.get_rect(topleft=(600, 420))
+StartGameR= StartGameB.get_rect(topleft=(150,420))
 # set the pygame window name
 pygame.display.set_caption('World War II simulator')
 
 
-SpriteList={StartGameB:(150, 420), LoadGameB:(600, 420), TitleScreen:(280, 100)}
+SpriteList={StartGameB:StartGameR, LoadGameB:LoadGameR}
+
 Background=TitleScreenStalin
-#def LoadSprites():
-    #for sprite in SpriteList:
-      #  Screen.blit(sprite, (0,0))
+
 
 def StartGame():
-    global Objects
     global TitleScreenHitler
     global TitleScreenStalin
     global LoadGameB
     global StartGameB
     global Background
     global WallpaperCycle
-    Objects.append([150, 420, 300, 100])
-    Objects.append([600, 420, 300, 100])
     while war==False:
         pygame.time.delay(1)
         if Tick()==0:
@@ -61,16 +54,11 @@ def StartGame():
                 Background = TitleScreenHitler
             elif WallpaperCycle == 2:
                 Background = TitleScreenStalin
-        for event in pygame.event.get():
-            if event.type == MOUSEBUTTONDOWN:
-                for j in Objects:
-                    print("no")
         #cycles through the different title screens
         DrawSprites()
         Events()
-        
-       # Screen.blit(TitleScreenThree, (0,0))
-        #time.sleep(5)
+
+
 def MousePos(coord):
     for event in pygame.event.get():
         if event.type == MOUSEBUTTONDOWN:
@@ -81,15 +69,15 @@ def MousePos(coord):
                 ret = pygame.mouse.get_pos()[1]
                 return ret
 def DrawSprites():
-    LoadAndStart = True
+#    LoadAndStart = True
     global Background
     global SpriteList
-    pygame.event.pump()
+    #pygame.event.pump()
     Screen.blit(Background, (0,0))
-    Screen.blit(LoadGameB, SpriteList.get(LoadGameB))
-    Screen.blit(StartGameB, SpriteList.get(StartGameB))
-    Screen.blit(TitleScreen, SpriteList.get(TitleScreen))
-    pygame.display.update()
+    for item in SpriteList:
+        rect=SpriteList.get(item)
+        coords=(rect[0],rect[1])
+        Screen.blit(item, coords)
 def Events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
